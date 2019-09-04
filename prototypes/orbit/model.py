546bffa1823f2a7b09255e23839ecb5a11b89080
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-09-04 18:33:30 -0300

    Improve readability

--- prototypes/orbit/model.py
+++ prototypes/orbit/model.py
@@ -4,22 +4,27 @@ import torch.nn.functional as F
 
 from torchdiffeq import odeint_adjoint as odeint
 
+
 class MLP(nn.Module):
     def __init__(self, input, layers, output):
         super(MLP, self).__init__()
         ds = [input] + layers + [output]
-        self.hidden_linear = [nn.Linear(d_i,d_o,bias=True) for d_i, d_o in zip(ds[:-2],ds[1:-1])]
-        self.output_linear = nn.Linear(ds[-2],ds[-1],bias=True)
+        self.hidden_linear = [nn.Linear(d_i, d_o, bias=True)
+                              for d_i, d_o in zip(ds[:-2], ds[1:-1])]
+        self.output_linear = nn.Linear(ds[-2], ds[-1], bias=True)
+
         seq = []
         for l in self.hidden_linear:
             seq.append(l)
             seq.append(nn.ReLU())
         seq.append(self.output_linear)
+
         self.mlp = nn.Sequential(*seq)
 
     def forward(self, x):
         return self.mlp(x)
-#end MLP
+# end MLP
+
 
 class IN(nn.Module):
     """
@@ -27,43 +32,46 @@ class IN(nn.Module):
       n is the number of objects
       m is the number of relations
       Msrc is the matrix indicating that object i is the source of relation r, of shape (n,m)
-      Mtgt is the matrix indicating that object i is the source of relation r, of shape (n,m)
+      Mtgt is the matrix indicating that object i is the target of relation r, of shape (n,m)
       O is the Object input matrix of shape (n,d_O)
       R is the Relation input matrix of shape (m,d_R)
       X is the external effect input matrix of shape (n,d_X)
-      
+
       There are two learned functions fR and fO.
       fR: (d_R+2*d_O) -> d_E
       fO: (d_E+d_X) -> d_P
-      
+
       Where
       E = fR(R'), the "effects" matrix, is of shape (m,d_E)
       P = fO(E'), the "prediction" matrix, is of shape (n,d_P)
       R' is the concatenation of R with Msrc.T x O and Mtgt.T x O, and is of shape (m,d_R+2*d_O)
       E' is the concatenation of Mtgt x E with X
     """
+
     def __init__(self, d_O, d_R, d_X, d_P):
         super(IN, self).__init__()
-        
+
         E_num_hidden_layers = 4
         d_E_hidden = 150
         d_E = 50
-        
+
         P_num_hidden_layers = 1
         d_P_hidden = 100
-        
+
         self.fR = MLP(d_R+2*d_O, E_num_hidden_layers*[d_E_hidden], d_E)
         self.fO = MLP(d_E+d_X, P_num_hidden_layers*[d_P_hidden], d_P)
 
     def forward(self, O, R, X, Msrc, Mtgt):
         Rsrc = torch.matmul(Msrc.t(), O)
         Rtgt = torch.matmul(Mtgt.t(), O)
-        R_prime = torch.cat( ([R] if R is not None else [])+[Rsrc,Rtgt], dim=1 )
+        R_prime = torch.cat(([R] if R is not None else [])+[Rsrc, Rtgt], dim=1)
         E = self.fR(R_prime)
-        E_prime = torch.cat( [torch.matmul(Mtgt,E)]+([X] if X is not None else []), dim=1)
+
+        E_prime = torch.cat([torch.matmul(Mtgt, E)] +
+                            ([X] if X is not None else []), dim=1)
         P = self.fO(E_prime)
         return P
-#end IN
+# end IN
 
 
 class IN_ODEfunc(nn.Module):
@@ -72,19 +80,19 @@ class IN_ODEfunc(nn.Module):
         E_num_hidden_layers = 4
         d_E_hidden = 150
         d_E = 50
-        
+
         P_num_hidden_layers = 1
         d_P_hidden = 100
-        
+
         # TODO: Allow relation and external objects
         d_X = 0
         d_R = 0
-        
+
         self.fR = MLP(d_R+2*d_O+1, E_num_hidden_layers*[d_E_hidden], d_E)
         self.fO = MLP(d_E+d_X, P_num_hidden_layers*[d_P_hidden], d_P)
         self.nfe = 0
-        
-    def set_fixed(self,Otail,Msrc,Mtgt):
+
+    def set_fixed(self, Otail, Msrc, Mtgt):
         # TODO: Allow relation and external objects
         self.Ofixed = Otail
         self.Msrc = Msrc
@@ -92,16 +100,18 @@ class IN_ODEfunc(nn.Module):
 
     def forward(self, t, x):
         self.nfe += 1
+
         # Concatenate time to node's features
-        O = torch.cat([x,self.Ofixed], dim=1)
+        O = torch.cat([x, self.Ofixed], dim=1)
         Rsrc = torch.matmul(self.Msrc.t(), O)
         Rtgt = torch.matmul(self.Mtgt.t(), O)
         tt = torch.ones_like(Rsrc[:, :1]) * t
-        R_prime = torch.cat([Rsrc,Rtgt,tt], dim=1)
+        R_prime = torch.cat([Rsrc, Rtgt, tt], dim=1)
         E = self.fR(R_prime)
-        E_prime = torch.matmul(self.Mtgt,E)
+        E_prime = torch.matmul(self.Mtgt, E)
         P = self.fO(E_prime)
         return P
+
 
 class IN_ODE(nn.Module):
     """
@@ -113,43 +123,45 @@ class IN_ODE(nn.Module):
       O is the Object input matrix of shape (n,d_O)
       R is the Relation input matrix of shape (m,d_R)
       X is the external effect input matrix of shape (n,d_X)
-      
+
       There are two learned functions fR and fO.
       fR: (d_R+2*d_O) -> d_E
       fO: (d_E+d_X) -> d_P
-      
+
       Where
       E = fR(R'), the "effects" matrix, is of shape (m,d_E)
       P = fO(E'), the "prediction" matrix, is of shape (n,d_P)
       R' is the concatenation of R with Msrc.T x O and Mtgt.T x O, and is of shape (m,d_R+2*d_O)
       E' is the concatenation of Mtgt x E with X
     """
-    def __init__(self, d_O, d_R, d_X, d_P,tol=1e-5):
+
+    def __init__(self, d_O, d_R, d_X, d_P, tol=1e-5):
         super(IN_ODE, self).__init__()
-        
+
         E_num_hidden_layers = 4
         d_E_hidden = 150
         d_E = 50
-        
+
         P_num_hidden_layers = 1
         d_P_hidden = 100
-        
+
         self.fR = MLP(d_R+2*d_O, E_num_hidden_layers*[d_E_hidden], d_E)
         self.fO = MLP(d_E+d_X, P_num_hidden_layers*[d_P_hidden], d_P)
         self.odefunc = IN_ODEfunc(d_O, d_R, d_X, d_P)
         self.integration_time = torch.tensor([0, 1]).float()
         self.tol = tol
-        
+
         self.d_P = d_P
 
-    def forward(self, O, R, X, Msrc, Mtgt,tol=None):
-        Otail = O[:,self.d_P:]
-        Ohead = O[:,:self.d_P]
+    def forward(self, O, R, X, Msrc, Mtgt, tol=None):
+        Otail = O[:, self.d_P:]
+        Ohead = O[:, :self.d_P]
         self.integration_time = self.integration_time.type_as(O)
-        self.odefunc.set_fixed(Otail,Msrc,Mtgt)
-        P = odeint(self.odefunc, Ohead, self.integration_time, rtol=self.tol, atol=self.tol)
+        self.odefunc.set_fixed(Otail, Msrc, Mtgt)
+        P = odeint(self.odefunc, Ohead, self.integration_time,
+                   rtol=self.tol, atol=self.tol)
         return P
-        
+
     @property
     def nfe(self):
         return self.odefunc.nfe
@@ -157,4 +169,4 @@ class IN_ODE(nn.Module):
     @nfe.setter
     def nfe(self, value):
         self.odefunc.nfe = value
-#end IN
+# end IN
--- prototypes/orbit/train_IN.py
+++ prototypes/orbit/train_IN.py
@@ -1,5 +1,7 @@
 import os
 
+from pprint import pprint as pp
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,74 +10,82 @@ import numpy as np
 
 from model import IN, IN_ODE
 
-TIMESTEP_TYPES = ["s","e"] #start and end
-VAR_NAMES = ["p","v","m","r","f","d"]
-VAR_FILENAMES = ["pos","vel","mass","radii","force","data"]
+TIMESTEP_TYPES = ["s", "e"]  # start and end
+VAR_NAMES = ["p", "v", "m", "r", "f", "d"]
+VAR_FILENAMES = ["pos", "vel", "mass", "radii", "force", "data"]
 
-def read_instance(data_folder,simulation,timestep):
+
+def read_instance(data_folder, simulation, timestep):
     i = {}
-    for ttype,tstep in zip(TIMESTEP_TYPES,[timestep,timestep+1]):
-        for var,fname in zip(VAR_NAMES,VAR_FILENAMES):
-            i[ttype+"_"+var] = np.load( "{folder}/{sim}/{tstep}.{var}.npy".format(
-                    folder = data_folder,
-                    sim = simulation,
-                    tstep = tstep,
-                    var = fname
-                    )
+    for ttype, tstep in zip(TIMESTEP_TYPES, [timestep, timestep+1]):
+        for var, fname in zip(VAR_NAMES, VAR_FILENAMES):
+            i[ttype+"_"+var] = np.load("{folder}/{sim}/{tstep}.{var}.npy".format(
+                folder=data_folder,
+                sim=simulation,
+                tstep=tstep,
+                var=fname
             )
-        #end for
-    #end for
-    return i
-#end read_instance
+            )
+        # end for
+    # end for
 
-def get_O(instance,ttype):
+    return i
+# end read_instance
+
+
+def get_O(instance, ttype):
     return np.concatenate(
         [instance[ttype+"_v"], instance[ttype+"_p"], instance[ttype+"_m"]],
-        axis = 1
+        axis=1
     )
-#end get_O
+# end get_O
+
 
 def process_instance(instance):
-    Oin = get_O(instance,"s")
-    Oout = get_O(instance,"e")
+    Oin = get_O(instance, "s")
+    Oout = get_O(instance, "e")
     float_dtype = Oout.dtype
+
     n = Oin.shape[0]
-    Adj_matrix = np.ones([n,n]) - np.eye(n)
-    relations = [(src,tgt) for src in range(n) for tgt in range(n) if Adj_matrix[src,tgt]!=0]
+    Adj_matrix = np.ones([n, n]) - np.eye(n)
+    relations = [(src, tgt) for src in range(n)
+                 for tgt in range(n) if Adj_matrix[src, tgt] != 0]
     m = len(relations)
-    Msrc = np.zeros([n,m],dtype=float_dtype)
-    Mtgt = np.zeros([n,m],dtype=float_dtype)
+
+    Msrc = np.zeros([n, m], dtype=float_dtype)
+    Mtgt = np.zeros([n, m], dtype=float_dtype)
+
     for r, st in enumerate(relations):
-        s,t = st
-        Msrc[s,r] = 1
-        Mtgt[t,r] = 1
+        s, t = st
+        Msrc[s, r] = 1
+        Mtgt[t, r] = 1
+
     return Oin, Oout, Msrc, Mtgt
-#end process_instance
-          
-          
+# end process_instance
+
+
 # TODO Make training schedule as in the paper
 # TODO Train and test for the 10 folds
-
 if __name__ == "__main__":
     DATA_FOLDER = "./data"
     MAX_TSTEP = 1000
     Model = IN
-    
-    simulations = sorted(os.listdir(DATA_FOLDER))
-    inputs = []
-    for sim in simulations:
-        for t in range(MAX_TSTEP-1):
-            inputs.append((sim,t))
-        #end for
-    #end for
+
+    simulations = sorted(
+        [x for x in os.listdir(DATA_FOLDER) if x != ".gitkeep"])
+    inputs = [(sim, t) for t in range(MAX_TSTEP-1) for sim in simulations]
+
     chosen = np.random.choice(len(inputs))
-    bla = read_instance(DATA_FOLDER,*inputs[chosen])
-    for k in bla:
-        print("{}: {}".format(k,bla[k]))
-    Oin, Oout, Msrc, Mtgt = process_instance(bla)
-    Oin, Oout, Msrc, Mtgt = map(lambda x: x.astype(np.float32), [Oin, Oout, Msrc, Mtgt])
-    for n,o in zip(["Oin","Oout","Msrc","Mtgt"],[Oin, Oout, Msrc, Mtgt]):
-        print(n,o.shape,o[0])
-    model = Model(Oin.shape[1],0,0,2)
-    y = model(torch.tensor(Oin), None, None, torch.tensor(Msrc), torch.tensor(Mtgt))
+    inst = read_instance(DATA_FOLDER, *inputs[chosen])
+    pp(inst)
+
+    Oin, Oout, Msrc, Mtgt = map(lambda x: x.astype(
+        np.float32), process_instance(inst))
+
+    for n, o in zip(["Oin", "Oout", "Msrc", "Mtgt"], [Oin, Oout, Msrc, Mtgt]):
+        print(n, o.shape, o[0])
+
+    model = Model(Oin.shape[1], 0, 0, 2)
+    y = model(torch.tensor(Oin), None, None,
+              torch.tensor(Msrc), torch.tensor(Mtgt))
     print(y)

e42bef0a91958d375c51432c14452b136f45d2c7
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-09-05 17:34:39 -0300

    Updated replay orbit simulation

--- prototypes/orbit/replay_simulation.py
+++ prototypes/orbit/replay_simulation.py
@@ -28,6 +28,7 @@ G = 39.478  # 6.67408e-11
 NUM_OF_BODIES = 6
 FLOAT_DTYPE = np.float
 SUN_INDEX = 0
+SUN_MASS = 100
 MIN_POSITION, MAX_POSITION = (10, 100)
 
 # Visualization constants
@@ -65,166 +66,123 @@ def nbody(dt, pos, vel, mass, radii=None, out_pos=None, out_vel=None,
     return out_pos, out_vel, force_placeholder
 
 
+def compute_radius(m):
+    r = np.log2(2 * m)
+
+    # Check sun(s) radius
+    if abs(m[SUN_INDEX, :] - SUN_MASS) < 0.001:
+        r[SUN_INDEX, :] = 2 * np.log2(np.sum(r[SUN_INDEX + 1:, :]))
+
+    return r
+
+
 def replay_simulation(folds: int = 10):
     """
     Replay a simulation saved.
     """
     REPLAY_FOLDER = './output'
-    for fold in trange(folds, desc="Folds"):
-        replay_fold = np.load("{}/{}".format(REPLAY_FOLDER, fold))
 
-        for scene in
+    for replay_file in os.listdir(REPLAY_FOLDER):
+        replay_np = np.load("{}/{}".format(REPLAY_FOLDER, replay_file))
 
-        # Run num_scenes simulations
-    for curr_scene in trange(start_at, start_at + int(num_scenes), desc="Scene"):
-        # Create save_data folder
-        if save_data:
-            os.makedirs("{}/{}".format(DATA_FOLDER, curr_scene), exist_ok=True)
+        pygame.init()
+        # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        # Generate values
-        v, v2, p, p2, m, f, d, c, r = generate_initial_values()
+        pygame.time.set_timer(TIME_EVENT_ID, int(1000*TIME_DELTA))
 
-        # Compute the orbit
-        v, p, m, r, c = compute_orbit(v, p, m, r, c, orbit_type)
+        # Configure variables to draw
+        delay = 0
+        radius = 0
 
         # Configure history
-        hp = np.empty((HIST_TIMESTEPS, NUM_OF_BODIES,
+        hp = np.empty((HIST_TIMESTEPS, replay_np[0][0].shape,
                        NUM_DIMS), dtype=np.float)
         hp[:] = np.nan
 
-        # Configure pyGame canvas
-        if draw:
-            pygame.init()
-            # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        c = np.random.randint(85, 255, size=(replay_np[0][0].shape, 3))
 
-            pygame.time.set_timer(TIME_EVENT_ID, int(1000*TIME_DELTA))
+        old_p = None
+        for vx, vy, px, py, m in replay_np:
+            r = compute_radius(m)
+            v = np.concatenate(vx[..., np.newaxis],
+                               vy[..., np.newaxis], axis=-1)
+            p = np.concatenate(px[..., np.newaxis],
+                               py[..., np.newaxis], axis=-1)
 
-            # Configure variables to draw
-            delay = 0
-            radius = 0
-            # Umax, Umin, passed_iter = float("-inf"), float("inf"), 0
+            redraw = False
+            while not redraw:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                        sys.exit()
+                    if event.type == TIME_EVENT_ID:
 
-        # Generate placeholders
-        all_p, all_v, all_m, all_r, all_f, all_data = np.zeros(
-            [max_timesteps, *p.shape]), np.zeros(
-            [max_timesteps, *v.shape]), np.zeros(
-            [max_timesteps, *m.shape]), np.zeros(
-            [max_timesteps, *r.shape]), np.zeros(
-            [max_timesteps, *f.shape]), np.zeros(
-            [max_timesteps, 1])
+                        # nbody(
+                        #     TIME_DELTA,
+                        #     p,
+                        #     v,
+                        #     m,
+                        #     out_pos=p2,
+                        #     out_vel=v2,
+                        #     force_placeholder=f,
+                        #     distance_placeholder=d,
+                        #     G=G
+                        # )
 
-        # Run this scene for max_timesteps steps
-        for curr_timestep in trange(max_timesteps, desc="Timestep"):
-            if draw:
-                redraw = False
-                while not redraw:
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                            sys.exit()
-                        if event.type == TIME_EVENT_ID:
-                            if save_data:
-                                for all_x, x in zip([all_p, all_v, all_m, all_r, all_f, all_data], [p, v, m, r, f, np.array([G])]):
-                                    all_x[curr_timestep] = x.copy()
+                        # # Swap position and velocities
+                        # p, p2 = p2, p
+                        # v, v2 = v2, v
 
-                            nbody(
-                                TIME_DELTA,
-                                p,
-                                v,
-                                m,
-                                out_pos=p2,
-                                out_vel=v2,
-                                force_placeholder=f,
-                                distance_placeholder=d,
-                                G=G
-                            )
+                        # End the loop, to be able to redraw the simulation
+                        redraw = True
+                    # end if
+                # end for
+            # end while
 
-                            # Swap position and velocities
-                            p, p2 = p2, p
-                            v, v2 = v2, v
+            # Update the timesteps
+            delay = (delay + 1) % TIMESTEP_DELAY
+            if not delay:
+                for t in range(HIST_TIMESTEPS-1):
+                    hp[t, :, :] = hp[t+1, :, :]
+            hp[HIST_TIMESTEPS-1, :, :] = p[:, :]
 
-                            # End the loop, to be able to redraw the simulation
-                            redraw = True
-                        # end if
-                    # end for
-                # end while
+            # Recompute the max_p and radius, used to configure zoom
+            avg_p = np.mean(p, axis=0)
+            max_p = np.max(np.linalg.norm(
+                p-avg_p[np.newaxis, :], axis=1))
+            radius = max(1.5 * max_p, radius)
+            # radius = 1.5*max_p # Uncomment for trippier visualisation
 
-                # Update the timesteps
-                delay = (delay + 1) % TIMESTEP_DELAY
-                if not delay:
-                    for t in range(HIST_TIMESTEPS-1):
-                        hp[t, :, :] = hp[t+1, :, :]
-                hp[HIST_TIMESTEPS-1, :, :] = p[:, :]
-
-                # Recompute the max_p and radius, used to configure zoom
-                avg_p = np.mean(p, axis=0)
-                if orbit_type == "circular" or orbit_type == "elliptical":
-                    avg_p = p[0]
-                max_p = np.max(np.linalg.norm(
-                    p-avg_p[np.newaxis, :], axis=1))
-                radius = max(1.5 * max_p, radius)
-                # radius = 1.5*max_p # Uncomment for trippier visualisation
-
-                if HEIGHT < WIDTH:
-                    h = 2 * radius
-                    w = h * WIDTH/HEIGHT
-                else:
-                    w = 2 * radius
-                    h = w * HEIGHT/WIDTH
-
-                # Redraw the planets/sun in the simulation
-                screen.fill(COLOR_BLACK)
-                for t in range(HIST_TIMESTEPS):
-                    for i in range(NUM_OF_BODIES):
-                        if not (np.isnan(hp[t, i])).any():
-                            pygame.gfxdraw.filled_circle(
-                                screen,
-                                int(WIDTH/2 +
-                                    (hp[t, i, 0] - avg_p[0]) * WIDTH/w),
-                                int(HEIGHT/2 + (hp[t, i, 1] -
-                                                avg_p[1]) * HEIGHT/h),
-                                int(max(1, r[i, 0] *
-                                        min(WIDTH, HEIGHT) / radius)),
-                                list(c[i]) + [255 // (HIST_TIMESTEPS-t)]
-                            )
-
-                # Flip color buffer
-                pygame.display.flip()
+            if HEIGHT < WIDTH:
+                h = 2 * radius
+                w = h * WIDTH/HEIGHT
             else:
-                if save_data:
-                    for all_x, x in zip([all_p, all_v, all_m, all_r, all_f, all_data], [p, v, m, r, f, np.array([G])]):
-                        all_x[curr_timestep] = x.copy()
+                w = 2 * radius
+                h = w * HEIGHT/WIDTH
 
-                nbody(
-                    TIME_DELTA,
-                    p,
-                    v,
-                    m,
-                    out_pos=p2,
-                    out_vel=v2,
-                    force_placeholder=f,
-                    distance_placeholder=d,
-                    G=G
-                )
+            # Redraw the planets/sun in the simulation
+            screen.fill(COLOR_BLACK)
+            for t in range(HIST_TIMESTEPS):
+                for i in range(NUM_OF_BODIES):
+                    if not (np.isnan(hp[t, i])).any():
+                        pygame.gfxdraw.filled_circle(
+                            screen,
+                            int(WIDTH/2 +
+                                (hp[t, i, 0] - avg_p[0]) * WIDTH/w),
+                            int(HEIGHT/2 + (hp[t, i, 1] -
+                                            avg_p[1]) * HEIGHT/h),
+                            int(max(1, r[i, 0] *
+                                    min(WIDTH, HEIGHT) / radius)),
+                            list(c[i]) + [255 // (HIST_TIMESTEPS-t)]
+                        )
 
-                # Swap position and velocities
-                p, p2 = p2, p
-                v, v2 = v2, v
-            # end if
+            # Flip color buffer
+            pygame.display.flip()
+
+            # Save old p
+            old_p = p.copy()
+
         # end for
-
-        # Save values
-        if save_data:
-            np.save("{}/{}/pos.npy".format(DATA_FOLDER, str(curr_scene)), all_p)
-            np.save("{}/{}/vel.npy".format(DATA_FOLDER, str(curr_scene)), all_v)
-            np.save("{}/{}/mass.npy".format(
-                DATA_FOLDER, str(curr_scene)), all_m)
-            np.save("{}/{}/radii.npy".format(
-                DATA_FOLDER, str(curr_scene)), all_r)
-            np.save("{}/{}/force.npy".format(
-                DATA_FOLDER, str(curr_scene)), all_f)
-            np.save("{}/{}/data.npy".format(
-                DATA_FOLDER, str(curr_scene)), all_data)
     # end for
 
 

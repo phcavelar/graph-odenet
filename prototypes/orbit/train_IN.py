2531b9c3707d8766f0ebb5013f8008f06006feb3
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-09-05 10:24:33 -0300

    Format orbit train_IN

--- prototypes/orbit/train_IN.py
+++ prototypes/orbit/train_IN.py
@@ -73,98 +73,114 @@ if __name__ == "__main__":
     DATASET_FOLDER = "./dataset/6"
     NUM_FOLDS = 10
     NUM_EPOCHS = 2000
-    BATCH_SIZE = 1000 #Higher since it only occupies about 1gb of VRAM
-    O_SHAPE = 5 # vx,vy,px,py,m
+    BATCH_SIZE = 1000  # Higher since it only occupies about 1gb of VRAM
+    O_SHAPE = 5  # vx,vy,px,py,m
     LEARNING_RATE = 0.001
     LR_DECAY = 0.8
     LR_DECAY_WINDOW = 40
-    VAL_LOSS_HIGH = 40.0 # High loss to initialise the decay window
-    L2_NORM = 5e-4 # Not provided in paper, took from our other experiments
-    PREDICTED_VALUES = 2 # vx, vy
-    NOISE_STD = 0.05 # We use 0.05 std instead of 0.05 of the dataset's std
+    VAL_LOSS_HIGH = 40.0  # High loss to initialise the decay window
+    L2_NORM = 5e-4  # Not provided in paper, took from our other experiments
+    PREDICTED_VALUES = 2  # vx, vy
+    NOISE_STD = 0.05  # We use 0.05 std instead of 0.05 of the dataset's std
     NOISE_PROPORTION_MAX = 0.2
     NOISE_EPOCH_START_DECAY = 50
     NOISE_EPOCH_STOP_DECAY = 250
     USE_CUDA = True
     use_cuda = USE_CUDA and torch.cuda.is_available()
     Model = IN
-    
-    dataset = np.load("{}/dataset.npy".format(DATASET_FOLDER)).astype(np.float32)
-    for fold in tqdm.trange(NUM_FOLDS, desc="Fold"):    
+
+    dataset = np.load("{}/dataset.npy".format(DATASET_FOLDER)
+                      ).astype(np.float32)
+    for fold in tqdm.trange(NUM_FOLDS, desc="Fold"):
         current_lr = LEARNING_RATE
         model = Model(O_SHAPE, 0, 0, PREDICTED_VALUES)
         if use_cuda:
             model = model.cuda()
         optimizer = optim.Adam(model.parameters(),
-                           lr=current_lr, weight_decay=L2_NORM)
-        train = np.load("{}/{}.train.npy".format(DATASET_FOLDER,fold))
-        test = np.load("{}/{}.test.npy".format(DATASET_FOLDER,fold))
-        validation = np.load("{}/{}.validation.npy".format(DATASET_FOLDER,fold))
-        validation_sch = np.ones(LR_DECAY_WINDOW)*VAL_LOSS_HIGH
+                               lr=current_lr, weight_decay=L2_NORM)
+        train = np.load("{}/{}.train.npy".format(DATASET_FOLDER, fold))
+        test = np.load("{}/{}.test.npy".format(DATASET_FOLDER, fold))
+        validation = np.load(
+            "{}/{}.validation.npy".format(DATASET_FOLDER, fold))
+        validation_sch = np.ones(LR_DECAY_WINDOW) * VAL_LOSS_HIGH
+
         for epoch in tqdm.trange(NUM_EPOCHS, desc="Epoch"):
             model.train()
-            for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset,train,BATCH_SIZE)), total=train.shape[0]/BATCH_SIZE, desc="Batch Train"):
+            for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset, train, BATCH_SIZE)), total=train.shape[0]/BATCH_SIZE, desc="Batch Train"):
                 # Random noise schedule
-                if False and epoch<NOISE_EPOCH_STOP_DECAY:
+                if False and epoch < NOISE_EPOCH_STOP_DECAY:
                     bOin, bOout, bMsrc, bMtgt, n_list, m_list = batch
-                    noise = np.random.normal(0,NOISE_STD,size=np.prod(bOin.shape))
+                    noise = np.random.normal(
+                        0, NOISE_STD, size=np.prod(bOin.shape))
                     # decay the proportion
                     proportion = NOISE_PROPORTION_MAX
-                    if epoch>NOISE_EPOCH_START_DECAY:
-                        proportion *= 1-((NOISE_EPOCH_STOP_DECAY-epoch)/(NOISE_EPOCH_STOP_DECAY-NOISE_EPOCH_START_DECAY))
-                    #end if
-                    idx = np.random.choice(noise.shape[0], replace=False, size=int(noise.shape[0] * (1-proportion)))
+                    if epoch > NOISE_EPOCH_START_DECAY:
+                        proportion *= 1 - \
+                            ((NOISE_EPOCH_STOP_DECAY-epoch) /
+                             (NOISE_EPOCH_STOP_DECAY-NOISE_EPOCH_START_DECAY))
+                    # end if
+                    idx = np.random.choice(noise.shape[0], replace=False, size=int(
+                        noise.shape[0] * (1-proportion)))
                     noise[idx] = 0
                     bOin += noise.reshape(bOin.shape)
                     batch = (bOin, bOout, bMsrc, bMtgt, n_list, m_list)
-                #end if
-                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(np.float32)) if not type(x) == list else torch.tensor(x,dtype=torch.float), batch)
+                # end if
+                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(
+                    np.float32)) if not type(x) == list else torch.tensor(x, dtype=torch.float), batch)
                 if use_cuda:
-                    bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
+                    bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(
+                        lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
                 Pred = model(bOin, None, None, bMsrc, bMtgt)
                 optimizer.zero_grad()
-                loss = F.mse_loss(Pred, bOout[:,:PREDICTED_VALUES])
+                loss = F.mse_loss(Pred, bOout[:, :PREDICTED_VALUES])
                 loss.backward()
                 optimizer.step()
-            #end for
-            
+            # end for
+
             model.eval()
             val_loss = []
-            for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset,validation,BATCH_SIZE)), total=validation.shape[0]/BATCH_SIZE, desc="Batch Valid"):
-                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(np.float32)) if not type(x) == list else torch.tensor(x,dtype=torch.float), batch)
+            for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset, validation, BATCH_SIZE)), total=validation.shape[0]/BATCH_SIZE, desc="Batch Valid"):
+                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(
+                    np.float32)) if not type(x) == list else torch.tensor(x, dtype=torch.float), batch)
                 if use_cuda:
-                    bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
+                    bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(
+                        lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
                 with torch.no_grad():
                     Pred = model(bOin, None, None, bMsrc, bMtgt)
-                    loss = F.mse_loss(Pred, bOout[:,:PREDICTED_VALUES])
-                #end with
+                    loss = F.mse_loss(Pred, bOout[:, :PREDICTED_VALUES])
+                # end with
                 val_loss.append(loss.cpu().item())
-            #end for
+            # end for
+
             val_loss = np.mean(val_loss)
             validation_sch[:-1] = validation_sch[1:]
             validation_sch[-1] = val_loss
-            if np.sum(validation_sch[1:]-validation_sch[:-1])>0:
-                current_lr*=LR_DECAY
+            if np.sum(validation_sch[1:]-validation_sch[:-1]) > 0:
+                current_lr *= LR_DECAY
                 optimizer = optim.Adam(model.parameters(),
-                           lr=current_lr, weight_decay=L2_NORM)
-            #end if
-        #end for
+                                       lr=current_lr, weight_decay=L2_NORM)
+            # end if
+        # end for
+
         model.eval()
         test_loss = []
-        for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset,test,BATCH_SIZE)), total=test.shape[0]/BATCH_SIZE, desc="Test"):
-            bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(np.float32)) if not type(x) == list else torch.tensor(x,dtype=torch.float), batch)
+        for b, batch in tqdm.tqdm(enumerate(get_epoch(dataset, test, BATCH_SIZE)), total=test.shape[0]/BATCH_SIZE, desc="Test"):
+            bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: torch.tensor(x.astype(
+                np.float32)) if not type(x) == list else torch.tensor(x, dtype=torch.float), batch)
             if use_cuda:
-                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
+                bOin, bOout, bMsrc, bMtgt, n_list, m_list = map(
+                    lambda x: x.cuda(), (bOin, bOout, bMsrc, bMtgt, n_list, m_list))
             with torch.no_grad():
                 Pred = model(bOin, None, None, bMsrc, bMtgt)
-                loss = F.mse_loss(Pred, bOout[:,:PREDICTED_VALUES])
-            #end with
+                loss = F.mse_loss(Pred, bOout[:, :PREDICTED_VALUES])
+            # end with
             test_loss.append(loss.cpu().item())
-        #end for
+        # end for
         test_loss = np.mean(val_loss)
         tqdm.tqdm.write(str(test_loss))
+
         # Saving/Loading TODO:
         # torch.save(model.state_dict(), PATH)
         # model = Model(*args, **kwargs)
         # model.load_state_dict(torch.load(PATH))
-    #end for
+    # end for
